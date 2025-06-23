4fe9c2f0172f79294bd9df793f33431c8293c2f7
KaraAliOsman <133917006+KaraAliOsman@users.noreply.github.com>
2025-06-22 22:55:00 -0400

    eeeeeeeeeee

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// 1️⃣  Línea de depuración (colócala aquí, sola)
 console.log("GEMINI ==> ", import.meta.env.VITE_GEMINI_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
